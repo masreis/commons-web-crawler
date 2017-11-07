cf38a23407b9de757b4d44bdc276b0856ee3c976
Marco Reis <ma@marcoreis.net>
2017-11-07 18:38:00 -0200

    update

--- src/main/java/net/marcoreis/commons/webcrawler/ExtratorConteudo.java
+++ src/main/java/net/marcoreis/commons/webcrawler/ExtratorConteudo.java
@@ -1,5 +1,9 @@
 package net.marcoreis.commons.webcrawler;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 import org.apache.log4j.Logger;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -9,28 +13,64 @@ import org.jsoup.select.Elements;
 public class ExtratorConteudo {
 	private static final Logger logger =
 			Logger.getLogger(ExtratorConteudo.class);
-	Document documento;
+	private Document documento;
 
-	public ExtratorConteudo(String pagina) {
-		documento = Jsoup.parse(pagina);
+	public void carregarConteudoHtml(String conteudoHtml) {
+		documento = Jsoup.parse(conteudoHtml);
 	}
 
-	public void extrairDadosHead() {
+	public void extraiConteudoHead() {
 		Element head = documento.head();
 		Elements elementos = head.children();
 		for (Element elemento : elementos) {
-			logger.info(elemento.tag());
+			logger.info(elemento.tagName());
+			// if (!elemento.tag().isEmpty()) {
+			// }
+			if (elemento.tag().isData()) {
+				logger.info("Data: " + elemento.data());
+			} else {
+				logger.info(elemento.hasText());
+				logger.info("Text: " + elemento.text());
+				logger.info("Val: " + elemento.val());
+				logger.info("Elemento: " + elemento);
+				logger.info(elemento.tag().isData());
+				logger.info(elemento.tag().isBlock());
+				logger.info(elemento.tag().isEmpty());
+				logger.info(elemento.tag().isInline());
+			}
+			logger.info("================");
+			// }
 		}
 	}
 
-	public void extrairConteudoPrincipal() {
+	public void extraiConteudoPrincipal() {
 		Element body = documento.body();
 		Element main = body.getElementById("main");
-		Elements categorias = body.getElementsByAttribute("rel");
+		Elements categorias = main.getElementsByAttributeValue(
+				"rel", "category tag");
 		logger.info(categorias.size());
 		for (Element e : categorias) {
-			logger.info(e.val());
+			logger.info(e.text());
 		}
-		logger.info(main.text());
+	}
+
+	public Set<String> extraiCategorias(
+			List<DumpNutchVO> listaVos) {
+		Set<String> lista = new HashSet<String>();
+		for (DumpNutchVO vo : listaVos) {
+			carregarConteudoHtml(vo.getContent());
+			Element body = documento.body();
+			Element main = body.getElementById("main");
+			if (main == null) {
+				continue;
+			}
+			Elements categorias =
+					main.getElementsByAttributeValue("rel",
+							"category tag");
+			for (Element e : categorias) {
+				lista.add(e.text());
+			}
+		}
+		return lista;
 	}
 }
--- src/test/java/net/marcoreis/commons/webcrawler/ExtratorConteudoTest.java
+++ src/test/java/net/marcoreis/commons/webcrawler/ExtratorConteudoTest.java
@@ -3,21 +3,50 @@ package net.marcoreis.commons.webcrawler;
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
+import java.util.Set;
 
+import org.apache.log4j.Logger;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class ExtratorConteudoTest {
-	private DumpNutchParser parser = new DumpNutchParser();
-	private File arquivoDump = new File(
-			"/home/marco/software/apache-nutch-1.13/crawlmr/dump-conteudo/dump");
+	private static List<DumpNutchVO> listaVos;
+	private static final Logger logger =
+			Logger.getLogger(ExtratorConteudoTest.class);
+
+	@BeforeClass
+	public static void iniciarlizar() throws IOException {
+		DumpNutchParser parser = new DumpNutchParser();
+		File arquivoDump = new File(
+				"/home/marco/software/apache-nutch-1.13/crawl/dump-conteudo/dump");
+		listaVos = parser.parse(arquivoDump);
+	}
+
+	// @Test
+	public void testExtraiConteudoPrincipal() {
+		for (DumpNutchVO vo : listaVos) {
+			ExtratorConteudo extrator = new ExtratorConteudo();
+			extrator.carregarConteudoHtml(vo.getContent());
+			extrator.extraiConteudoPrincipal();
+		}
+	}
+
+	// @Test
+	public void testExtraiCategorias() {
+		ExtratorConteudo extrator = new ExtratorConteudo();
+		Set<String> categorias =
+				extrator.extraiCategorias(listaVos);
+		for (String categoria : categorias) {
+			logger.info(categoria);
+		}
+	}
 
 	@Test
-	public void testExtrator() throws IOException {
-		List<DumpNutchVO> lista = parser.parse(arquivoDump);
-		for (DumpNutchVO vo : lista) {
-			ExtratorConteudo extrator =
-					new ExtratorConteudo(vo.getContent());
-			extrator.extrairConteudoPrincipal();
+	public void testExtraiConteudoHead() {
+		for (DumpNutchVO vo : listaVos) {
+			ExtratorConteudo extrator = new ExtratorConteudo();
+			extrator.carregarConteudoHtml(vo.getContent());
+			extrator.extraiConteudoHead();
 		}
 	}
 }
